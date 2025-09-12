5a7c5ae1e8841ae5b4a21d47bda4ae86e43947af
allanmckenzie <allan.mckenzie@hmcts.net>
2025-09-12 13:51:04 +0100

    PEG-2415: Remove 'VALIDATE_EVENTS' jmx command

--- event-store-management/event-store-management-command-handler-extension/src/main/java/uk/gov/justice/services/eventstore/management/validation/process/MissingSchemaIdException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.services.eventstore.management.validation.process;
-
-public class MissingSchemaIdException extends RuntimeException {
-
-    public MissingSchemaIdException(final String message) {
-        super(message);
-    }
-}
