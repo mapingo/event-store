550839da2f5f1e4f4e89635b1711cb99e7503f8d
allanmckenzie <allan.mckenzie@hmcts.net>
2025-09-12 15:11:47 +0100

    PEG-2415: Remove REBUILD and VERIFY_REBUILD jmx commands

--- event-store-management/event-store-management-core/src/main/java/uk/gov/justice/services/eventstore/management/verification/commands/EventStoreVerificationCommandHandler.java
+++ event-store-management/event-store-management-core/src/main/java/uk/gov/justice/services/eventstore/management/verification/commands/EventStoreVerificationCommandHandler.java
@@ -1,10 +1,8 @@
 package uk.gov.justice.services.eventstore.management.verification.commands;
 
 import static uk.gov.justice.services.eventstore.management.commands.VerifyCatchupCommand.VERIFY_CATCHUP;
-import static uk.gov.justice.services.eventstore.management.commands.VerifyRebuildCommand.VERIFY_REBUILD;
 
 import uk.gov.justice.services.eventstore.management.commands.VerifyCatchupCommand;
-import uk.gov.justice.services.eventstore.management.commands.VerifyRebuildCommand;
 import uk.gov.justice.services.eventstore.management.verification.process.EventStoreVerification;
 import uk.gov.justice.services.jmx.api.parameters.JmxCommandRuntimeParameters;
 import uk.gov.justice.services.jmx.command.HandlesSystemCommand;
@@ -19,16 +17,6 @@ public class EventStoreVerificationCommandHandler {
 
     @Inject
     private EventStoreVerification eventStoreVerification;
-
-    @Interceptors(MdcLoggerInterceptor.class)
-    @HandlesSystemCommand(VERIFY_REBUILD)
-    public void verifyRebuild(
-            final VerifyRebuildCommand verifyRebuildCommand,
-            final UUID commandId,
-            @SuppressWarnings("unused")
-            final JmxCommandRuntimeParameters jmxCommandRuntimeParameters) {
-        eventStoreVerification.verifyEventStore(commandId, verifyRebuildCommand);
-    }
 
     @Interceptors(MdcLoggerInterceptor.class)
     @HandlesSystemCommand(VERIFY_CATCHUP)
